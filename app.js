23ac90c3be533a12742efea52d552607521ea341
Aayush shrestha <aayushrestha5@gmail.com>
2023-05-22 23:00:14 +0545

    first commit

--- app.js
+++ app.js
@@ -41,10 +41,18 @@ app.post("/", function(req,res){
 
     const options={
         method: "POST", 
-        auth: "aayush1:c06a176942eebd070fdc6dca8b8b46cf-us10" // look documentation for option object
+        auth: "aayush1:a6e0338e5d20d068cfb4630cd3a014fc4-us10" // look documentation for option object
     }
 
     const request = https.request(url, options, function(response){
+        // checking if statuscode = 200 then it means success or nothing is wrong so can be proceed else not.
+
+        if (response.statusCode===200){
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname +
+                "/failure.html");
+        }
         response.on("data", function(data){
             console.log(JSON.parse(data));
         })
@@ -54,9 +62,13 @@ app.post("/", function(req,res){
 
 });
 
-app.listen(3000, function(){
+app.post("/failure", function(req, res){
+    res.redirect("/");
+}); // for redirecting to home page using try again button
+
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running on port 3000");
-});
+}); // process.env.PORT will automatically fill up the port in which heroku will host
 
 
 // API key
